e338b11125668829197eef13d021718c3c626333
Albino Marcos Acosta <marcosalbinoacosta@gmail.com>
2026-05-14 17:36:44 -0300

    fix: hardcode projectId in sanity CLI config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,8 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_DATASET ?? 'production',
+    projectId: '276wtn1s',
+    dataset: 'production',
   },
   studioHost: 'niza',
 });
